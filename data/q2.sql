de072261b569f2cbbd2ea8b93609b01476844000
w2ak <w2ak@users.noreply.github.com>
2016-11-25 17:00:17 +0100

    query 2

--- data/q2.sql
+++ data/q2.sql
@@ -1,26 +1,30 @@
-WITH morpheus AS (
-    SELECT (person_id,movie_id)
+-- Question 2
+-- Find the name of each actor who played the character Morpheus in a video
+-- game, together with the name of the game.
+-- Order the results by year in descending order, then by title in ascending
+-- order.
+WITH morpheus_id AS (
+    SELECT id
+    FROM char_name
+    WHERE name = 'Morpheus'
+),
+video_game AS (
+    SELECT id
+    FROM movie_type
+    WHERE kind = 'video game'
+),
+movie_with_morpheus AS (
+    SELECT person_id,movie_id
     FROM cast_info
-    WHERE (person_role_id = ???) #look at char_name id equivalent to Morpheus
+    INNER JOIN morpheus_id
+            ON morpheus_id.id = cast_info.person_role_id
 )
-
-WITH video_game AS (
-    SELECT (id AS movie_id,production_year,title)
-    FROM movie
-    WHERE (kind_id =???) look at corresponding in movie_type for video game
-)
-
-WITH both_conditions AS (
-    SELECT (A.person_id,B.title)
-    FROM morpheus A 
-         INNER JOIN video_game B
-            ON A.movie_id = B.movie_id
-    ORDER BY B.production_year DESC, B.title ASC
-)
-
-SELECT (A.name,B.title)
-FROM person A
-    INNER JOIN both_conditions B
-        ON A.id = B.person_id;
-
-
+SELECT person.name,title
+      FROM movie
+INNER JOIN video_game
+        ON video_game.id = movie.kind_id
+INNER JOIN movie_with_morpheus
+        ON movie_with_morpheus.movie_id = movie.id
+INNER JOIN person
+        ON person.id = person_id
+ORDER BY production_year DESC, title ASC;
