b604991d20039283d30a7132ca361637faf978a8
w2ak <w2ak@users.noreply.github.com>
2016-11-27 00:24:27 +0100

    query 3

--- data/q3.sql
+++ data/q3.sql
@@ -1,34 +1,25 @@
 -- Question 3
 -- Find the name of all the people who played in a movie they directed, and
 -- order them by their name.
--- WITH directors_actors AS (
-    -- SELECT person_id,movie_id
-    -- FROM cast_info
-    -- WHERE (role_id = 1)
-
-    -- INTERSECT
-
-    -- SELECT person_id,movie_id
-    -- FROM cast_info
-    -- WHERE (role_id = 8)
--- )
--- SELECT name
--- FROM person
--- INNER JOIN directors_actors
--- ON directors_actors.person_id=person.id
--- ORDER BY name ASC;
-
-
+WITH actors AS (
+  SELECT person_id,movie_id
+  FROM cast_info
+  WHERE role_id = 1
+),
+directors AS (
+  SELECT person_id,movie_id
+  FROM cast_info
+  WHERE role_id = 8
+),
+"both" AS (
+  SELECT DISTINCT actors.person_id
+  FROM actors
+  INNER JOIN directors
+  ON actors.movie_id = directors.movie_id
+  AND actors.person_id = directors.person_id
+)
 SELECT person.name
-FROM cast_info AS A
-INNER JOIN role_type AS RA
-ON RA.id = A.role_id
-INNER JOIN cast_info AS B
-ON A.movie_id = B.movie_id
-AND A.person_id = B.person_id
-INNER JOIN role_type AS RB
-ON RB.id = B.role_id
-INNER JOIN person
-ON person.id = A.person_id
-WHERE RA.role = 'actor'
-AND RB.role = 'director';
+FROM person
+INNER JOIN "both"
+ON "both".person_id = person.id
+ORDER BY person.name ASC;
